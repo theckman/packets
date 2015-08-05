78a2e9edb64a4780d3570288b72c695377a55a8e
Tim Heckman <t@heckman.io>
2015-08-04 20:22:19 -0700

    improve package docs

--- doc.go
+++ doc.go
@@ -2,6 +2,6 @@
 // Use of this source code is governed by the BSD 3-Clause
 // license that can be found in the LICENSE file.
 
-// Package packets is a package for the creation/manipulation of raw TCP packets for sending over the network.
+// Package packets is for the creation/manipulation of raw TCP packets for sending over the network.
 // It was designed to have a idiomatic (or so I think) interface.
 package packets
